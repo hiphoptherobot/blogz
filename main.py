e24ef324163b66fd4b311b59018cf02e48f31981
Ben Hoelzer <hiphoptherobot23@gmail.comgit config --global user.name Ben>
2018-10-30 16:22:14 -0500

    Finishing web ceasar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
+import cgi
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -7,83 +8,55 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:hotdog@loc
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
-
 class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
-    blog_post = db.Column(db.String(500))
-
-    def __init__(self, title_arg,blog_post_arg):
-        self.title = title_arg
-        self.blog_post = blog_post_arg
-
-@app.route('/')
-def homepage():
-    return redirect('/blog')
-    
+    body = db.Column(db.String(300))
+        
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+               
 @app.route('/blog', methods=['POST', 'GET'])
-def index():
-    if request.args.get('id') is not None:
-        id_value = int(request.args.get('id'))
-        singleblog = Blog.query.filter_by(id = id_value).one()
-        return render_template('singleblog.html',title="Build-a-blog",eachblog=singleblog)
-    else:
-        blog_list = Blog.query.all()
-        return render_template('blogs.html',title="Build-a-blog", blogs = blog_list)
+def blog():
+    blogs = Blog.query.all()
+    return render_template('blog.html', blogs=blogs)
 
-@app.route('/newpost')
-def new_post():
-    return render_template('newpost.html',title="Build-a-blog")
+@app.route('/newpost', methods=['POST', 'GET'])
+def newpost():
+    
+    if request.method == 'POST':
 
-@app.route('/newpost',methods=['POST'])
-def submitform():
-    title_name_form = request.form['form-title']
-    error_titlename = validatetitlename(title_name_form)
-
-    blog_post_form = request.form['form-blog']
-    error_blogpost = validateblogpost(blog_post_form)
-
-    if len(error_titlename) == 0 and len(error_blogpost)== 0:
-        new_blog = Blog(title_name_form,blog_post_form)
-        db.session.add(new_blog)
+        title = request.form['title']
+        body = request.form['body']
+        if len(title) < 1 or len(body) < 1:
+            error = "Please enter a title or body"
+            return render_template('newpost.html', error=error) 
+                         
+        title = request.form['title']
+        body = request.form['body']
+        id = request.args.get('id')
+        newpost = Blog(title, body)
+        db.session.add(newpost)
         db.session.commit()
-        return redirect('/blog?id={0}'.format(new_blog.id))
-    else:
-        return render_template('newpost.html',title="Build-a-blog!",
-            title_error=error_titlename,blog_error=error_blogpost,
-            title_namefield=title_name_form,blog_postfield=blog_post_form)
+        id = newpost.id
+        id = str(id)                                          
+        return redirect("/post?id=" + id)
 
-def validatetitlename(title_name_form):
-    if len (title_name_form ) == 0:
-        return "Title cannot be Empty!"
-    elif len(title_name_form)>120:
-        return "Title cannot be longer than 120 characters!"
-    else:
-        return ""
+       
+    return render_template('newpost.html')
 
-def validateblogpost(blog_post_form):
-    if len (blog_post_form ) == 0:
-        return "Blog cannot be Empty!"
-    elif len(blog_post_form)>500:
-        return "Blog cannot be longer than 500 characters!"
-    else:
-        return ""
-
-
-
-
-#@app.route('/delete-task', methods=['POST'])
-#def delete_task():
-
-#    task_id = int(request.form['task-id'])
-#    task = Task.query.get(task_id)
-#   task.completed = True
-#  db.session.add(task)
-#   db.session.commit()
-
-#   return redirect('/')
-
+@app.route('/post', methods=['GET'])
+def post():
+    id = request.args.get('id')               
+    submitted_blogs = Blog.query.filter_by(id=id).all() 
+    return render_template('post.html', submitted_blogs=submitted_blogs)
+                      
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    encoded_error = request.args.get("error")
+    return render_template('blog.html',title="Build A Blog", error=encoded_error and cgi.escape(encoded_error, quote=True))
 
 if __name__ == '__main__':
     app.run()
